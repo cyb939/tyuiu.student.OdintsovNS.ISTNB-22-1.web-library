9ac35f884f8361070217ada8a4d931932878217f
cyb939 <nikita.70odintsov@mail.ru>
2023-11-11 11:53:26 +0300

    конса

---
+++ tyuiu.student.OdintsovNS.ISTNB-22-1.web-library/Controllers/RegBookController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tyuiu.student.OdintsovNS.ISTNB_22_1.web_library.DataModels;
+using tyuiu.student.OdintsovNS.ISTNB_22_1.web_library.Models;
+
+namespace tyuiu.student.OdintsovNS.ISTNB_22_1.web_library.Controllers
+{
+    public class RegBookController : Controller
+    {
+        private MyDBContext context;
+
+        public IActionResult Index()
+        {
+            ViewBag["Person"] = context.Persons.AsEnumerable();
+            return View(new RegBook { Books = context.Books,Rooms = context.Room });
+        }
+        public RegBookController(MyDBContext context)
+        {
+            this.context = context;
+        }
+    }
+}
---
+++ tyuiu.student.OdintsovNS.ISTNB-22-1.web-library/Models/RegBook.cs
@@ -0,0 +1,10 @@
+﻿using tyuiu.student.OdintsovNS.ISTNB_22_1.web_library.DataModels;
+
+namespace tyuiu.student.OdintsovNS.ISTNB_22_1.web_library.Models
+{
+    public class RegBook
+    {
+        public IEnumerable<Book> Books;
+        public IEnumerable<Room> Rooms;
+    }
+}
